c1d96baf3e7ca139f6b47537b44edba11a52b8e0
fr2i <fredoprog@gmail.com>
2015-03-03 10:27:31 +0100

    commit intermediaire
    ajout dans une base mongoDB

--- Exercice5/index.rb
+++ Exercice5/index.rb
@@ -5,12 +5,16 @@
 require 'open-uri'
 require 'zlib'
 require 'yajl'
+require 'mongo'
+
+connexionMongo ==  Mongo::Connection.new
+db = mongo['githubarchive']
  
 gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
 js = Zlib::GzipReader.new(gz).read
  
 Yajl::Parser.parse(js) do |event|
-  print event
+  db['events'] << event 
 end
 
 
