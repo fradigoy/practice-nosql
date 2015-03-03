ec37989c37b98b5b273cdcf3508148198342f3fa
fr2i <fredoprog@gmail.com>
2015-03-03 10:21:10 +0100

    Commit intermediaire

--- Exercice5/index.rb
+++ Exercice5/index.rb
@@ -1,3 +1,16 @@
 
 
-# Classe de recuppération 
+# Classe de recuppération activity sur github un jour j 
+
+require 'open-uri'
+require 'zlib'
+require 'yajl'
+ 
+gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
+js = Zlib::GzipReader.new(gz).read
+ 
+Yajl::Parser.parse(js) do |event|
+  print event
+end
+
+
