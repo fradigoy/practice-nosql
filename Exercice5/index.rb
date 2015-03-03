fe0bed726da1dc57eba0429d879c2bf161c169a8
fr2i <fredoprog@gmail.com>
2015-03-03 10:50:15 +0100

    commit intermediaire
    commit modif chargement pdt 24h activity github

--- Exercice5/index.rb
+++ Exercice5/index.rb
@@ -1,19 +1,38 @@
 
 
 # Classe de recuppération activity sur github un jour j 
+# Modification : recupération d'activity de github sur 24h
 
 require 'open-uri'
 require 'zlib'
 require 'yajl'
 require 'mongo'
 
+
+#connexionMongo =  Mongo::Connection.new
+#db = connexionMongo['githubarchive']
+ 
+#gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
+#js = Zlib::GzipReader.new(gz).read
+ 
+#Yajl::Parser.parse(js) do |event|
+#  db['events'] << event 
+#end
+
+
 connexionMongo =  Mongo::Connection.new
 db = connexionMongo['githubarchive']
+
+
+{1...23}.each do |hour|
+	puts "..... hour #{hour}"
  
-gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
+gz = open('http://data.githubarchive.org/2015-01-01-12-#{hour}.json.gz')
 js = Zlib::GzipReader.new(gz).read
  
 Yajl::Parser.parse(js) do |event|
+
+  event['create_at'] = Time.parse(event[create_at])
   db['events'] << event 
 end
 
